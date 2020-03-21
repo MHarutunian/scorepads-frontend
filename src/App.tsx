ab36bc752dcabe8caee6a1a81dc6f50449c87b21
Matteo Harutunian <matteo.harutunian@netlight.com>
2020-03-21 18:19:54 +0100

    Added header including basic routing using react-router.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Route,
+  Switch
+} from 'react-router-dom';
 
 import List from './UserManagement/List';
+import Header from './Header';
 
 function App() {
   return (
-    <List />
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route path="/users">
+          <List />
+        </Route>
+        <Route path="/doppelkopf">
+          <h2>TODO</h2>
+        </Route>
+        <Route path="/jank">
+          <h2>TODO</h2>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
