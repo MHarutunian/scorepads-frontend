aeddd25d594a314c544040cafdc6e4ac40fe34a2
Matteo Harutunian <matteo.harutunian@netlight.com>
2020-04-13 16:47:22 +0200

    Added styled-components to the application.

---
+++ src/Header/ui.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import { Link as RouterLink } from 'react-router-dom';
+
+// eslint-disable-next-line import/prefer-default-export
+export const Link = styled(RouterLink)`
+  margin: 0 10px;
+  padding: 10px;
+  background-color: #f4bc42;
+  border-radius: 15px;
+`;
