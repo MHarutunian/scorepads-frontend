32058e4f70cb747bdc41cb5c72f9cb406eec944d
Matteo Harutunian <matteo@harutunian.de>
2020-05-10 12:15:44 +0200

    Add tests for useApi hook, fix tests for App component.

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,6 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+
+jest.spyOn(window, 'fetch').mockImplementation(
+  () => Promise.reject(new Error('Fetch was called without being mocked')));
