c3f46620a04ec10adc8700ea4f7a93c9dd491912
LAPTOP-I96RF18Q\marco <marco.wilms@live.de>
2020-11-16 20:23:41 +0100

    excluded tests for term list from app test

--- src/App/__tests__/App.test.tsx
+++ src/App/__tests__/App.test.tsx
@@ -6,22 +6,10 @@ import {
 
 import App from '../App';
 
-// TODO: Clean this up once we move the /players API call to its own component
-jest.spyOn(window, 'fetch').mockImplementation(() => new Promise<Response>(() => { }));
-
 describe('App', () => {
   it('renders header by default', () => {
     const { getByText } = render(<App />);
     expect(getByText('Scorepads')).toBeInTheDocument();
-  });
-
-  it('renders user management when switching to it', () => {
-    const { getByText } = render(<App />);
-
-    const userManagementLink = getByText('Spielerverwaltung');
-    fireEvent.click(userManagementLink);
-
-    expect(getByText('List')).toBeInTheDocument();
   });
 
   it('renders Doppelkopf page when switching to it', () => {
@@ -29,15 +17,6 @@ describe('App', () => {
 
     const doppelkopfLink = getByText('Doppelkopf');
     fireEvent.click(doppelkopfLink);
-
-    expect(getByText('TODO')).toBeInTheDocument();
-  });
-
-  it('renders JanK page when switching to it', () => {
-    const { getByText } = render(<App />);
-
-    const jankLink = getByText('JanK');
-    fireEvent.click(jankLink);
 
     expect(getByText('TODO')).toBeInTheDocument();
   });
