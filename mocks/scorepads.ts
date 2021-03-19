8564cc0951ba2c024ae61caed23792ac231d82d3
LAPTOP-I96RF18Q\marco <marco.wilms@live.de>
2021-03-19 17:15:10 +0100

    added mock data of scorepads

---
+++ mocks/scorepads.ts
@@ -0,0 +1,32 @@
+export default [
+  {
+    _id: '1',
+    game: 'Jank',
+    players: [{
+      _id: '1',
+      name: 'Matteo',
+      picture: '',
+    },
+    {
+      _id: '2',
+      name: 'Anna',
+      picture: '',
+    }],
+    createdAt: '2021-02-07T16:22:55.000Z',
+  },
+  {
+    _id: '2',
+    game: 'Jank',
+    players: [{
+      _id: '1',
+      name: 'Matteo',
+      picture: '',
+    },
+    {
+      _id: '3',
+      name: 'Marco',
+      picture: '',
+    }],
+    createdAt: '2021-02-09T16:22:55.000Z',
+  },
+];
