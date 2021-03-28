3e22ec5208282c36ac62727f2bf18f85dfb5aa81
LAPTOP-I96RF18Q\marco <marco.wilms@live.de>
2021-03-28 19:32:09 +0200

    sorted scorepads by date

--- src/hooks/useScorepads.ts
+++ src/hooks/useScorepads.ts
@@ -18,24 +18,32 @@ export interface ApiScorepad {
 
 const useScorepads = (game: string): [Error | null, Scorepad[] | null] => {
   const [error, apiScorepads] = useApi<ApiScorepad[]>(`/scorepads/?game=${game}`);
-  const scorepads = useMemo(() => apiScorepads && apiScorepads.map((scorepad: ApiScorepad) => ({
-    // eslint-disable-next-line no-underscore-dangle
-    id: scorepad._id,
-    game: scorepad.game,
-    players: scorepad.players.map((player: ApiPlayer) => ({
+  const scorepads = useMemo(() => {
+    if (!apiScorepads) {
+      return null;
+    }
+    return apiScorepads.map((scorepad: ApiScorepad) => ({
       // eslint-disable-next-line no-underscore-dangle
-      id: player._id,
-      name: player.name,
-      picture: player.picture,
-    })),
-    date: scorepad.created_at,
-  })), [apiScorepads]);
+      id: scorepad._id,
+      game: scorepad.game,
+      players: scorepad.players.map((player: ApiPlayer) => ({
+        // eslint-disable-next-line no-underscore-dangle
+        id: player._id,
+        name: player.name,
+        picture: player.picture,
+      })),
+      date: scorepad.created_at,
+    })).sort(
+      (scorepadA, scorepadB) => {
+        if (scorepadB.date === scorepadA.date) {
+          return 0;
+        }
+        return scorepadB.date < scorepadA.date ? -1 : 1;
+      },
+    );
+  }, [apiScorepads]);
 
-  const sortedScorepads = useMemo(() => scorepads
-    && scorepads.sort((scorepadA, scorepadB) => (scorepadB.date < scorepadA.date ? -1 : 1
-    )), [scorepads]);
-
-  return [error, sortedScorepads];
+  return [error, scorepads];
 };
 
 export default useScorepads;
