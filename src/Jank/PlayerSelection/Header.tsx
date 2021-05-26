fd36edf9bc22ec8bae9ed7a1588957960885e612
LAPTOP-I96RF18Q\marco <marco.wilms@live.de>
2021-05-26 18:15:47 +0200

    removed unneccesary fragments

--- src/Jank/PlayerSelection/Header.tsx
+++ src/Jank/PlayerSelection/Header.tsx
@@ -2,11 +2,9 @@ import Menu from '../../Menu/Menu';
 import MenuLink from '../../Menu/MenuLink';
 
 const Header = () => (
-  <>
-    <Menu>
-      <MenuLink to="/jank">Zurück zur Spieleauswahl</MenuLink>
-    </Menu>
-  </>
+  <Menu>
+    <MenuLink to="/jank">Zurück zur Spieleauswahl</MenuLink>
+  </Menu>
 );
 
 export default Header;
