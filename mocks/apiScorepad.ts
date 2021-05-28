6dd9164a3f375aace5fec1e8fd4d71684c738594
LAPTOP-I96RF18Q\marco <marco.wilms@live.de>
2021-05-28 11:54:51 +0200

    removed unneccesary mocks

--- mocks/apiScorepad.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  _id: '1',
-  game: 'JanK',
-  players: [],
-  created_at: '2020-04-30T18:07:55.000Z',
-};
--- mocks/scorepad.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  id: '1',
-  game: 'JanK',
-  players: [],
-  date: '2020-04-30T18:07:55.000Z',
-};
