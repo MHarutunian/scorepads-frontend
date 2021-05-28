7f94ca65bf61f7e39f006730c5bde76db5ecb38e
LAPTOP-I96RF18Q\marco <marco.wilms@live.de>
2021-05-28 11:55:34 +0200

    defined mocks directly in tests

--- src/services/__tests__/map.service.test.ts
+++ src/services/__tests__/map.service.test.ts
@@ -1,19 +1,31 @@
 import {
   mapPlayers, mapTerms, mapScorepad, mapScorepads,
 } from '../map.service';
-import apiPlayers from '../../../mocks/apiPlayers';
-import players from '../../../mocks/players';
-import apiTerms from '../../../mocks/apiTerms';
-import terms from '../../../mocks/terms';
-import apiScorepad from '../../../mocks/apiScorepad';
-import scorepad from '../../../mocks/scorepad';
-import apiScorepads from '../../../mocks/apiScorepads';
-import scorepads from '../../../mocks/scorepads';
 
 describe('players mapper', () => {
   it('returns a mapped player object', async () => {
-    const expectedResult = players;
-    const unmappedPlayers = apiPlayers;
+    const expectedResult = [{
+      id: '42',
+      name: 'Matteo',
+      picture: '',
+    },
+    {
+      id: '12',
+      name: 'Marco',
+      picture: '',
+    }];
+    const unmappedPlayers = [
+      {
+        _id: '42',
+        name: 'Matteo',
+        picture: '',
+      },
+      {
+        _id: '12',
+        name: 'Marco',
+        picture: '',
+      },
+    ];
     const result = mapPlayers(unmappedPlayers);
 
     expect(result).toStrictEqual(expectedResult);
@@ -22,8 +34,34 @@ describe('players mapper', () => {
 
 describe('terms mapper', () => {
   it('returns a mapped terms object', async () => {
-    const expectedResult = terms;
-    const unmappedTerms = apiTerms;
+    const expectedResult = [
+      {
+        _id: '13',
+        value: 'First Term',
+      },
+      {
+        _id: '37',
+        value: 'Second Term',
+      },
+      {
+        _id: '42',
+        value: 'Third Term',
+      },
+    ];
+    const unmappedTerms = [
+      {
+        _id: '13',
+        value: 'First Term',
+      },
+      {
+        _id: '37',
+        value: 'Second Term',
+      },
+      {
+        _id: '42',
+        value: 'Third Term',
+      },
+    ];
     const result = mapTerms(unmappedTerms);
 
     expect(result).toStrictEqual(expectedResult);
@@ -32,8 +70,26 @@ describe('terms mapper', () => {
 
 describe('scorepad mapper', () => {
   it('returns a mapped scorepad object', async () => {
-    const expectedResult = scorepad;
-    const unmappedScorepad = apiScorepad;
+    const expectedResult = {
+      id: '1',
+      game: 'JanK',
+      players: [{
+        id: '1',
+        name: 'Matteo',
+        picture: '',
+      }],
+      date: '2020-04-30T18:07:55.000Z',
+    };
+    const unmappedScorepad = {
+      _id: '1',
+      game: 'JanK',
+      players: [{
+        _id: '1',
+        name: 'Matteo',
+        picture: '',
+      }],
+      created_at: '2020-04-30T18:07:55.000Z',
+    };
     const result = mapScorepad(unmappedScorepad);
 
     expect(result).toStrictEqual(expectedResult);
@@ -42,8 +98,70 @@ describe('scorepad mapper', () => {
 
 describe('scorepads mapper', () => {
   it('returns a mapped scorepads object', async () => {
-    const expectedResult = scorepads;
-    const unmappedScorepads = apiScorepads;
+    const expectedResult = [
+      {
+        id: '2',
+        game: 'Jank',
+        players: [{
+          id: '1',
+          name: 'Matteo',
+          picture: '',
+        },
+        {
+          id: '3',
+          name: 'Marco',
+          picture: '',
+        }],
+        date: '2021-02-09T16:22:55.000Z',
+      },
+      {
+        id: '1',
+        game: 'Jank',
+        players: [{
+          id: '1',
+          name: 'Matteo',
+          picture: '',
+        },
+        {
+          id: '2',
+          name: 'Anna',
+          picture: '',
+        }],
+        date: '2021-02-07T16:22:55.000Z',
+      },
+    ];
+    const unmappedScorepads = [
+      {
+        _id: '1',
+        game: 'Jank',
+        players: [{
+          _id: '1',
+          name: 'Matteo',
+          picture: '',
+        },
+        {
+          _id: '2',
+          name: 'Anna',
+          picture: '',
+        }],
+        created_at: '2021-02-07T16:22:55.000Z',
+      },
+      {
+        _id: '2',
+        game: 'Jank',
+        players: [{
+          _id: '1',
+          name: 'Matteo',
+          picture: '',
+        },
+        {
+          _id: '3',
+          name: 'Marco',
+          picture: '',
+        }],
+        created_at: '2021-02-09T16:22:55.000Z',
+      },
+    ];
     const result = mapScorepads(unmappedScorepads);
 
     expect(result).toStrictEqual(expectedResult);
